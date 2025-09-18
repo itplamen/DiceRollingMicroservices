bff99287505c743987d766c942aa69158e6e4024
itplamen <itplamen@gmail.com>
2025-09-18 16:32:17 +0300

    Created command handler for registering users

---
+++ DiceRollingMicroservices/UserDataService/Handlers/UserDataAccessService.Handlers.Commands/Register/RegisterUserCommand.cs
@@ -0,0 +1,19 @@
+﻿namespace UserDataAccessService.Handlers.Commands.Register
+{
+    using MediatR;
+
+    using UserDataAccessService.Handlers.Commands.Response;
+
+    public class RegisterUserCommand  : IRequest<BaseResponse>
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ DiceRollingMicroservices/UserDataService/Handlers/UserDataAccessService.Handlers.Commands/Register/RegisterUserCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿namespace UserDataAccessService.Handlers.Commands.Register
+{
+    using AutoMapper;
+
+    using MediatR;
+
+    using Microsoft.AspNetCore.Identity;
+
+    using UserDataAccessService.Data.Models;
+    using UserDataAccessService.Handlers.Commands.Response;
+
+    public class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, BaseResponse>
+    {
+        private readonly IMapper mapper;
+        private readonly UserManager<User> userManager;
+ 
+        public RegisterUserCommandHandler(IMapper mapper, UserManager<User> userManager)
+        {
+            this.mapper = mapper;
+            this.userManager = userManager;
+        }
+
+        public async Task<BaseResponse> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+        {
+            User user = await userManager.FindByEmailAsync(request.Email);
+
+            if (user == null)
+            {
+                User newUser = mapper.Map<User>(request);
+                IdentityResult result = await userManager.CreateAsync(newUser, request.Password);
+                BaseResponse response = mapper.Map<BaseResponse>(result);
+
+                return response;
+            }
+
+            return new BaseResponse("Username already exists");
+        }
+    }
+}
