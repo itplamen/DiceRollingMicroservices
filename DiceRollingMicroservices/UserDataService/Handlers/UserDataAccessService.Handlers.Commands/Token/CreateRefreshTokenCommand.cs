ba42f021e3b6caab83bd281ebbe6e43aba5a6adc
itplamen <itplamen@gmail.com>
2025-09-18 18:01:31 +0300

    Created command and handler for refresh token

---
+++ DiceRollingMicroservices/UserDataService/Handlers/UserDataAccessService.Handlers.Commands/Token/CreateRefreshTokenCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace UserDataAccessService.Handlers.Commands.Token
+{
+    using MediatR;
+
+    using UserDataAccessService.Handlers.Commands.Response;
+
+    public class CreateRefreshTokenCommand : IRequest<TokenResponse>
+    {
+        public string RefreshToken { get; set; }
+    }
+}
---
+++ DiceRollingMicroservices/UserDataService/Handlers/UserDataAccessService.Handlers.Commands/Token/CreateRefreshTokenCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿namespace UserDataAccessService.Handlers.Commands.Token
+{
+    using MediatR;
+
+    using UserDataAccessService.Data.Models;
+    using UserDataAccessService.Handlers.Commands.Response;
+    using UserDataAccessService.Handlers.Queries.Token;
+
+    public class CreateRefreshTokenCommandHandler : IRequestHandler<CreateRefreshTokenCommand, TokenResponse>
+    {
+        private readonly IMediator mediator;
+
+        public async Task<TokenResponse> Handle(CreateRefreshTokenCommand request, CancellationToken cancellationToken)
+        {
+            RefreshToken refreshToken = await mediator.Send(new GetTokenQuery() { Token = request.RefreshToken }, cancellationToken);
+
+            if (refreshToken != null)
+            {
+                await mediator.Send(new RevokeTokenCommand(refreshToken), cancellationToken);
+                TokenResponse newToken = await mediator.Send(new CreateAccessTokenCommand(refreshToken.UserId, refreshToken.User.Email), cancellationToken);
+
+                return newToken;
+            }
+
+            return new TokenResponse("Refresh token does not exist!");
+        }
+    }
+}
