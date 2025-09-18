485e24d37ed57ff776990a6f2f89500bb3d1a94e
itplamen <itplamen@gmail.com>
2025-09-18 16:33:36 +0300

    Created command handler for user login

---
+++ DiceRollingMicroservices/UserDataService/Handlers/UserDataAccessService.Handlers.Commands/Login/LoginUserCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace UserDataAccessService.Handlers.Commands.Login
+{
+    using MediatR;
+
+    using UserDataAccessService.Handlers.Commands.Response;
+
+    public class LoginUserCommand : IRequest<TokenResponse>
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
---
+++ DiceRollingMicroservices/UserDataService/Handlers/UserDataAccessService.Handlers.Commands/Login/LoginUserCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿namespace UserDataAccessService.Handlers.Commands.Login
+{
+    using System.Threading.Tasks;
+
+    using AutoMapper;
+    
+    using MediatR;
+    
+    using Microsoft.AspNetCore.Identity;
+    
+    using UserDataAccessService.Data.Models;
+    using UserDataAccessService.Handlers.Commands.Response;
+    using UserDataAccessService.Handlers.Commands.Token;
+
+    public class LoginUserCommandHandler : IRequestHandler<LoginUserCommand, TokenResponse>
+    {
+        private readonly IMapper mapper;
+        private readonly IMediator mediator;
+        private readonly UserManager<User> userManager;
+
+        public LoginUserCommandHandler(IMapper mapper, IMediator mediator, UserManager<User> userManager)
+        {
+            this.mapper = mapper;
+            this.mediator = mediator;
+            this.userManager = userManager;
+        }
+
+        public async Task<TokenResponse> Handle(LoginUserCommand request, CancellationToken cancellationToken)
+        {
+            User user = await userManager.FindByEmailAsync(request.Email);
+
+            if (user != null)
+            {
+                bool isPasswordValid = await userManager.CheckPasswordAsync(user, request.Password);
+
+                if (isPasswordValid)
+                {
+                    TokenResponse response = await mediator.Send(new CreateTokenCommand() { Email = request.Email, UserId = user.Id });
+                    return response;
+                }
+
+                return new TokenResponse("Invalid password");
+            }
+
+            return new TokenResponse("Username does not exists");
+        }
+    }
+}
