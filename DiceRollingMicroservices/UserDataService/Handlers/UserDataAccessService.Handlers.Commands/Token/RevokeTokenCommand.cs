a8b7b5b0ca7313c14889562206748a705551b59f
itplamen <itplamen@gmail.com>
2025-09-18 16:35:04 +0300

    Created command handler for JWT token revoke

---
+++ DiceRollingMicroservices/UserDataService/Handlers/UserDataAccessService.Handlers.Commands/Token/RevokeTokenCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace UserDataAccessService.Handlers.Commands.Token
+{
+    using MediatR;
+
+    using UserDataAccessService.Data.Models;
+
+    public class RevokeTokenCommand : IRequest
+    {
+        public RefreshToken RefreshToken { get; set; }
+    }
+}
---
+++ DiceRollingMicroservices/UserDataService/Handlers/UserDataAccessService.Handlers.Commands/Token/RevokeTokenCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace UserDataAccessService.Handlers.Commands.Token
+{
+    using MediatR;
+
+    using UserDataAccessService.Data.Contracts;
+    using UserDataAccessService.Data.Models;
+
+    public class RevokeTokenCommandHandler : IRequestHandler<RevokeTokenCommand>
+    {
+        private readonly IRepository<RefreshToken> repository;
+
+        public RevokeTokenCommandHandler(IRepository<RefreshToken> repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task Handle(RevokeTokenCommand request, CancellationToken cancellationToken)
+        {
+            repository.Delete(request.RefreshToken);
+            await repository.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
