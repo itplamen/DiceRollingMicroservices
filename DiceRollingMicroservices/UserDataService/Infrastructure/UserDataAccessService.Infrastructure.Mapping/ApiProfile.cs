51986b375854cd0659bb7bc66c13209ae6c87e64
itplamen <itplamen@gmail.com>
2025-09-18 18:08:31 +0300

    Created IoC mapping profiles

---
+++ DiceRollingMicroservices/UserDataService/Infrastructure/UserDataAccessService.Infrastructure.Mapping/ApiProfile.cs
@@ -0,0 +1,32 @@
+﻿namespace UserDataAccessService.Infrastructure.Mapping
+{
+    using AutoMapper;
+
+    using UserDataAccessService.Api.Models;
+    using UserDataAccessService.Handlers.Commands.Login;
+    using UserDataAccessService.Handlers.Commands.Register;
+    using UserDataAccessService.Handlers.Commands.Token;
+
+    public sealed class ApiProfile : Profile
+    {
+        public ApiProfile()
+        {
+            CreateMap<RegisterRequest, RegisterUserCommand>()
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dest => dest.Password, opt => opt.MapFrom(src => src.Password))
+                .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FirstName))
+                .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName))
+                .ForMember(dest => dest.ImageUrl, opt => opt.MapFrom(src => src.ImageUrl));
+
+            CreateMap<AuthRequest, LoginUserCommand>()
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dest => dest.Password, opt => opt.MapFrom(src => src.Password));
+
+            CreateMap<RefreshTokenRequest, CreateRefreshTokenCommand>()
+                .ForMember(dest => dest.RefreshToken, opt => opt.MapFrom(src => src.RefreshToken));
+
+            CreateMap<RefreshTokenRequest, RevokeTokenCommand>()
+                .ForMember(dest => dest.RefreshToken, opt => opt.MapFrom(src => src.RefreshToken));
+        }
+    }
+}
---
+++ DiceRollingMicroservices/UserDataService/Infrastructure/UserDataAccessService.Infrastructure.Mapping/HandlersProfile.cs
@@ -0,0 +1,26 @@
+﻿namespace UserDataAccessService.Infrastructure.Mapping
+{
+    using AutoMapper;
+    
+    using Microsoft.AspNetCore.Identity;
+    
+    using UserDataAccessService.Data.Models;
+    using UserDataAccessService.Handlers.Commands.Register;
+    using UserDataAccessService.Handlers.Commands.Response;
+
+    public class HandlersProfile : Profile
+    {
+        public HandlersProfile()
+        {
+            CreateMap<RegisterUserCommand, User>()
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FirstName))
+                .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName))
+                .ForMember(dest => dest.ImageUrl, opt => opt.MapFrom(src => src.ImageUrl));
+
+            CreateMap<IdentityResult, BaseResponse>()
+                .ForMember(dest => dest.IsSuccess, opt => opt.MapFrom(src => src.Succeeded))
+                .ForMember(dest => dest.Errors, opt => opt.MapFrom(src => src.Errors.Select(e => e.Description).ToList()));
+        }
+    }
+}
