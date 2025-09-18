19c0547352d4d416e897f33a7f4e341a6659b2e4
itplamen <itplamen@gmail.com>
2025-09-18 16:28:25 +0300

    Created query handler for retrieving refresh token

---
+++ DiceRollingMicroservices/UserDataService/Handlers/UserDataAccessService.Handlers.Queries/Token/GetTokenQuery.cs
@@ -0,0 +1,11 @@
+﻿namespace UserDataAccessService.Handlers.Queries.Token
+{
+    using MediatR;
+
+    using UserDataAccessService.Data.Models;
+
+    public class GetTokenQuery : IRequest<RefreshToken>
+    {
+        public string Token { get; set; }
+    }
+}
---
+++ DiceRollingMicroservices/UserDataService/Handlers/UserDataAccessService.Handlers.Queries/Token/GetTokenQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿namespace UserDataAccessService.Handlers.Queries.Token
+{
+    using MediatR;
+    
+    using Microsoft.EntityFrameworkCore;
+
+    using UserDataAccessService.Data.Contracts;
+    using UserDataAccessService.Data.Models;
+
+    public class GetTokenQueryHandler : IRequestHandler<GetTokenQuery, RefreshToken>
+    {
+        private readonly IRepository<RefreshToken> repository;
+
+        public GetTokenQueryHandler(IRepository<RefreshToken> repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<RefreshToken> Handle(GetTokenQuery request, CancellationToken cancellationToken)
+        {
+           var refreshToken = await repository.Filter()
+                .Include(x => x.User)
+                .FirstOrDefaultAsync(x => 
+                    x.Token == request.Token && x.ExpiryDate > DateTime.UtcNow && x.DeletedOn == null, 
+                    cancellationToken);
+
+            return refreshToken;
+        }
+    }
+}
