4a1e8ab40a506a740cfa953a82b8678f82ae5b3a
itplamen <itplamen@gmail.com>
2025-09-18 23:53:41 +0300

    Fixed configurations

--- DiceRollingMicroservices/UserDataService/Api/UserDataAccessService.Api/Program.cs
+++ DiceRollingMicroservices/UserDataService/Api/UserDataAccessService.Api/Program.cs
@@ -1,6 +1,12 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using UserDataAccessService.Data;
+using UserDataAccessService.Data.Models;
+using UserDataAccessService.Infrastructure.IoC;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +16,24 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<UserDataServiceDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        b => b.MigrationsAssembly("UserDataAccessService.Data")
+    )
+);
+
+builder.Services.AddIdentity<User, Role>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+})
+.AddEntityFrameworkStores<UserDataServiceDbContext>()
+.AddDefaultTokenProviders();
+
+builder.Services.AddServices(builder.Configuration);
 
 var app = builder.Build();
 
