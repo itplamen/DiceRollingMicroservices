dabd5a20580b8c79a7d91a01b6bc96969fce6063
itplamen <itplamen@gmail.com>
2025-09-20 01:46:50 +0300

    Created predicate builder extension

---
+++ DiceRollingMicroservices/OperativeService/Infrastructure/OperativeService.Infrastructure.Utils/Extensions/PredicateBuilder.cs
@@ -0,0 +1,21 @@
+﻿namespace OperativeService.Infrastructure.Utils.Extensions
+{
+    using System.Linq.Expressions;
+
+    public static class PredicateBuilder
+    {
+        public static Expression<Func<T, bool>> True<T>() => f => true;
+
+        public static Expression<Func<T, bool>> And<T>(
+            this Expression<Func<T, bool>> firstExpr,
+            Expression<Func<T, bool>> secondExpr)
+        {
+            var parameter = Expression.Parameter(typeof(T));
+            var combined = Expression.AndAlso(
+                Expression.Invoke(firstExpr, parameter),
+                Expression.Invoke(secondExpr, parameter));
+
+            return Expression.Lambda<Func<T, bool>>(combined, parameter);
+        }
+    }
+}
