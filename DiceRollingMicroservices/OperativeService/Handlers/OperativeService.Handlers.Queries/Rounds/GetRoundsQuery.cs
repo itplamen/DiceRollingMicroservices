451977739e52c4183011eb5fb2c020674d6709df
itplamen <itplamen@gmail.com>
2025-09-20 01:47:51 +0300

    Created query for retrieving rounds

---
+++ DiceRollingMicroservices/OperativeService/Handlers/OperativeService.Handlers.Queries/Rounds/GetRoundsQuery.cs
@@ -0,0 +1,11 @@
+﻿namespace OperativeService.Handlers.Queries.Rounds
+{
+    using MediatR;
+    
+    using OperativeService.Data.Models;
+
+    public class GetRoundsQuery : IRequest<IEnumerable<Round>>
+    {
+        public string GameId { get; set; }
+    }
+}
---
+++ DiceRollingMicroservices/OperativeService/Handlers/OperativeService.Handlers.Queries/Rounds/GetRoundsQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace OperativeService.Handlers.Queries.Rounds
+{
+    using MediatR;
+    
+    using OperativeService.Data.Contracts;
+    using OperativeService.Data.Models;
+
+    public class GetRoundsQueryHandler : IRequestHandler<GetRoundsQuery, IEnumerable<Round>>
+    {
+        private readonly IRepository<Round> repository;
+
+        public GetRoundsQueryHandler(IRepository<Round> repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<IEnumerable<Round>> Handle(GetRoundsQuery request, CancellationToken cancellationToken) =>
+            await repository.FindAsync(x => x.GameId == request.GameId && x.DeletedOn == null, cancellationToken);
+    }
+}
