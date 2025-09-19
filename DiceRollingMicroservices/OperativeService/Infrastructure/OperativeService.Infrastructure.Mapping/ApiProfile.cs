b4da2ce55b7686b9671e6373e6cfaa007a86412d
itplamen <itplamen@gmail.com>
2025-09-20 02:45:08 +0300

    Created mapping profiles

---
+++ DiceRollingMicroservices/OperativeService/Infrastructure/OperativeService.Infrastructure.Mapping/ApiProfile.cs
@@ -0,0 +1,27 @@
+﻿namespace OperativeService.Infrastructure.Mapping
+{
+    using AutoMapper;
+    
+    using OperativeService.Api.Models;
+    using OperativeService.Data.Models;
+    using OperativeService.Handlers.Commands.Common;
+    using OperativeService.Handlers.Commands.Games;
+
+    public class ApiProfile : Profile
+    {
+        public ApiProfile()
+        {
+            CreateMap<CreateGameRequest, CreateGameCommand>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.DicePerUser, opt => opt.MapFrom(src => src.DicePerUser))
+                .ForMember(dest => dest.MaxRounds, opt => opt.MapFrom(src => src.MaxRounds))
+                .ForMember(dest => dest.MaxUsers, opt => opt.MapFrom(src => src.MaxUsers))
+                .ForMember(dest => dest.DieType, opt => opt.MapFrom(src => Enum.Parse<DieType>(src.DieType)))
+                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId));
+
+            CreateMap<JoinGameRequest, GameCommand>()
+                .ForMember(dest => dest.GameId, opt => opt.MapFrom(src => src.GameId))
+                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId));
+        }
+    }
+}
---
+++ DiceRollingMicroservices/OperativeService/Infrastructure/OperativeService.Infrastructure.Mapping/HandlersProfile.cs
@@ -0,0 +1,27 @@
+﻿namespace OperativeService.Infrastructure.Mapping
+{
+    using AutoMapper;
+ 
+    using OperativeService.Data.Models;
+    using OperativeService.Handlers.Commands.Common;
+    using OperativeService.Handlers.Commands.Games;
+    using OperativeService.Handlers.Queries.Games;
+
+    public class HandlersProfile : Profile
+    {
+        public HandlersProfile()
+        {
+            CreateMap<CreateGameCommand, Game>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.DieType, opt => opt.MapFrom(src => src.DieType))
+                .ForMember(dest => dest.MaxUsers, opt => opt.MapFrom(src => src.MaxUsers))
+                .ForMember(dest => dest.MaxRounds, opt => opt.MapFrom(src => src.MaxRounds))
+                .ForMember(dest => dest.DicePerUser, opt => opt.MapFrom(src => src.DicePerUser))
+                .ForMember(dest => dest.UserIds, opt => opt.MapFrom(src => new HashSet<string>() { src.UserId }));
+
+            CreateMap<GameCommand, GetAvailableGamesQuery>()
+                .ForMember(dest => dest.GameId, opt => opt.MapFrom(src => src.GameId))
+                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId));
+        }
+    }
+}
