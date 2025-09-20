56537f02e3117070266ec493dae281d0e2d2679d
itplamen <itplamen@gmail.com>
2025-09-20 03:47:15 +0300

    Fixed JWT token validation

--- DiceRollingMicroservices/OperativeService/Api/OperativeService.Api/Program.cs
+++ DiceRollingMicroservices/OperativeService/Api/OperativeService.Api/Program.cs
@@ -1,26 +1,48 @@
+using System.Text;
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+
 using OperativeService.Data;
 using OperativeService.Infrastructure.IoC;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddSingleton<OperativeServiceDbContext>();
 builder.Services.AddSingleton<DbInitializer>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
+builder.Services.AddAuthorization();
 
 builder.Services.AddServices();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -33,10 +55,12 @@ using (var scope = app.Services.CreateScope())
     dbInitializer.Init();
 }
 
+//app.UseHttpsRedirection();
 
-app.UseHttpsRedirection();
-
+app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
+app.MapControllers();
 
 app.MapControllers();
 
--- DiceRollingMicroservices/UserDataService/Api/UserDataAccessService.Api/Program.cs
+++ DiceRollingMicroservices/UserDataService/Api/UserDataAccessService.Api/Program.cs
@@ -4,16 +4,14 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
 using UserDataAccessService.Data;
 using UserDataAccessService.Data.Models;
 using UserDataAccessService.Infrastructure.IoC;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -37,7 +35,6 @@ builder.Services.AddServices();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
