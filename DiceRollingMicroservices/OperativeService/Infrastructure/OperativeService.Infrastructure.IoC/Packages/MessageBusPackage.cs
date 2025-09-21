89b08b8f487d97b66b3c43cb68fb1ca579feaff8
itplamen <itplamen@gmail.com>
2025-09-21 17:43:04 +0300

    Added service bus IoC packages

---
+++ DiceRollingMicroservices/OperativeService/Infrastructure/OperativeService.Infrastructure.IoC/Packages/MessageBusPackage.cs
@@ -0,0 +1,44 @@
+﻿namespace OperativeService.Infrastructure.IoC.Packages
+{
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    
+    using RabbitMQ.Client;
+
+    using DiceRollingMicroservices.Common.Models.IoC;
+    using DiceRollingMicroservices.MessageBus.Consumer;
+    using DiceRollingMicroservices.MessageBus.Consumer.Contracts;
+    
+    public sealed class MessageBusPackage : IPackage
+    {
+        private readonly IConfiguration configuration;
+
+        public MessageBusPackage(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public void RegisterServices(IServiceCollection services)
+        {
+            services.AddSingleton<IEventProcessor, EventProcessor>();
+            services.AddSingleton<IConnectionFactory, ConnectionFactory>(x =>
+            {
+                return new ConnectionFactory
+                {
+                    HostName = configuration["RabbitMQ:Host"],
+                    Port = int.Parse(configuration["RabbitMQ:Port"])
+                };
+            });
+
+            services.AddHostedService(x =>
+            {
+                var eventProcessor = x.GetRequiredService<IEventProcessor>();
+                var connectionFactory = x.GetRequiredService<IConnectionFactory>();
+                string exchangeName = configuration["RabbitMQ:Exchange"];
+                string queueName = configuration["RabbitMQ:Queue"];
+
+                return new MessageBusSubscriber(eventProcessor, connectionFactory, exchangeName, queueName);
+            });
+        }
+    }
+}
---
+++ DiceRollingMicroservices/UserDataService/Infrastructure/UserDataAccessService.Infrastructure.IoC/Packages/MessageBusPackage.cs
@@ -0,0 +1,41 @@
+﻿namespace UserDataAccessService.Infrastructure.IoC.Packages
+{
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    using RabbitMQ.Client;
+
+    using DiceRollingMicroservices.Common.Models.IoC;
+    using DiceRollingMicroservices.MessageBus.Producer;
+    using DiceRollingMicroservices.MessageBus.Producer.Contracts;
+    using DiceRollingMicroservices.MessageBus.Models;
+
+    public sealed class MessageBusPackage : IPackage
+    {
+        private readonly IConfiguration configuration;
+
+        public MessageBusPackage(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public void RegisterServices(IServiceCollection services)
+        {
+            services.AddSingleton<IMessageBusClient<UserMsg>>(sp =>
+            {
+                var connectionFactory = sp.GetRequiredService<IConnectionFactory>();
+                var exchangeName = configuration["RabbitMQ:Exchange"];
+
+                return new MessageBusClient<UserMsg>(connectionFactory, exchangeName);
+            });
+            services.AddSingleton<IConnectionFactory, ConnectionFactory>(x =>
+            {
+                return new ConnectionFactory
+                {
+                    HostName = configuration["RabbitMQ:Host"],
+                    Port = int.Parse(configuration["RabbitMQ:Port"])
+                };
+            });
+        }
+    }
+}
